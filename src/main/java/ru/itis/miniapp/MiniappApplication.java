5f1e4f21875040ddb2eac95990006b03131d8268
ZhenShen.ITIS <ewgenii20066@gmail.com>
2025-04-06 20:57:39 +0300

    maven create project

---
+++ src/main/java/ru/itis/miniapp/MiniappApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.miniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniappApplication.class, args);
+	}
+
+}
