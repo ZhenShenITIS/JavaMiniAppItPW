b23340122f087bae205154ec7837608c6480beda
ZhenShen.ITIS <ewgenii20066@gmail.com>
2025-04-06 21:06:00 +0300

    test commit

--- src/main/java/ru/itis/miniapp/MiniappApplication.java
+++ src/main/java/ru/itis/miniapp/MiniappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MiniappApplication.class, args);
+		SpringApplication.run(MiniappApplication.class, args); //test
 	}
 
 }
